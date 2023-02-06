406c47f3034d05ee4ed394d26a84a3c15c027a88
cagim125 <cagim30@gmail.com>
2023-02-06 16:00:18 +0900

    Initial commit

---
+++ src/main/java/com/omg/springtestproject/controller/DeleteController.java
@@ -0,0 +1,16 @@
+package com.omg.springtestproject.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/delete-api")
+public class DeleteController {
+
+    // http://localhost:8080/api/v1/delete-api/variable1/{String 값}
+    @DeleteMapping(value = "/delete/{variable}")
+    public String DeleteVariable(@PathVariable String variable) { return variable; }
+
+}
